93b29e830e5fb45fbd84818a2ac34bcfba8c57a7
VasiliBaranov <vasili.baranov@gmail.com>
2017-05-16 19:34:33 +0200

    Removed icc builds, updated test gcc builds

--- _ReleaseTests/PackingGeneration/Generation/PackingServices/PostProcessing/Source/subdir.mk
+++ _ReleaseTests/PackingGeneration/Generation/PackingServices/PostProcessing/Source/subdir.mk
@@ -4,28 +4,46 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
+../PackingGeneration/Generation/PackingServices/PostProcessing/Source/EquilibrationPressureProcessor.cpp \
+../PackingGeneration/Generation/PackingServices/PostProcessing/Source/ErrorRateProcessor.cpp \
 ../PackingGeneration/Generation/PackingServices/PostProcessing/Source/HessianService.cpp \
 ../PackingGeneration/Generation/PackingServices/PostProcessing/Source/InsertionRadiiGenerator.cpp \
+../PackingGeneration/Generation/PackingServices/PostProcessing/Source/IntermediateScatteringFunctionProcessor.cpp \
+../PackingGeneration/Generation/PackingServices/PostProcessing/Source/MinIterationsProcessor.cpp \
 ../PackingGeneration/Generation/PackingServices/PostProcessing/Source/MolecularDynamicsService.cpp \
 ../PackingGeneration/Generation/PackingServices/PostProcessing/Source/OrderService.cpp \
 ../PackingGeneration/Generation/PackingServices/PostProcessing/Source/PressureService.cpp \
-../PackingGeneration/Generation/PackingServices/PostProcessing/Source/RattlerRemovalService.cpp 
+../PackingGeneration/Generation/PackingServices/PostProcessing/Source/RattlerRemovalService.cpp \
+../PackingGeneration/Generation/PackingServices/PostProcessing/Source/ScatterAndDiffusionProcessor.cpp \
+../PackingGeneration/Generation/PackingServices/PostProcessing/Source/SelfDiffusionProcessor.cpp 
 
 OBJS += \
+./PackingGeneration/Generation/PackingServices/PostProcessing/Source/EquilibrationPressureProcessor.o \
+./PackingGeneration/Generation/PackingServices/PostProcessing/Source/ErrorRateProcessor.o \
 ./PackingGeneration/Generation/PackingServices/PostProcessing/Source/HessianService.o \
 ./PackingGeneration/Generation/PackingServices/PostProcessing/Source/InsertionRadiiGenerator.o \
+./PackingGeneration/Generation/PackingServices/PostProcessing/Source/IntermediateScatteringFunctionProcessor.o \
+./PackingGeneration/Generation/PackingServices/PostProcessing/Source/MinIterationsProcessor.o \
 ./PackingGeneration/Generation/PackingServices/PostProcessing/Source/MolecularDynamicsService.o \
 ./PackingGeneration/Generation/PackingServices/PostProcessing/Source/OrderService.o \
 ./PackingGeneration/Generation/PackingServices/PostProcessing/Source/PressureService.o \
-./PackingGeneration/Generation/PackingServices/PostProcessing/Source/RattlerRemovalService.o 
+./PackingGeneration/Generation/PackingServices/PostProcessing/Source/RattlerRemovalService.o \
+./PackingGeneration/Generation/PackingServices/PostProcessing/Source/ScatterAndDiffusionProcessor.o \
+./PackingGeneration/Generation/PackingServices/PostProcessing/Source/SelfDiffusionProcessor.o 
 
 CPP_DEPS += \
+./PackingGeneration/Generation/PackingServices/PostProcessing/Source/EquilibrationPressureProcessor.d \
+./PackingGeneration/Generation/PackingServices/PostProcessing/Source/ErrorRateProcessor.d \
 ./PackingGeneration/Generation/PackingServices/PostProcessing/Source/HessianService.d \
 ./PackingGeneration/Generation/PackingServices/PostProcessing/Source/InsertionRadiiGenerator.d \
+./PackingGeneration/Generation/PackingServices/PostProcessing/Source/IntermediateScatteringFunctionProcessor.d \
+./PackingGeneration/Generation/PackingServices/PostProcessing/Source/MinIterationsProcessor.d \
 ./PackingGeneration/Generation/PackingServices/PostProcessing/Source/MolecularDynamicsService.d \
 ./PackingGeneration/Generation/PackingServices/PostProcessing/Source/OrderService.d \
 ./PackingGeneration/Generation/PackingServices/PostProcessing/Source/PressureService.d \
-./PackingGeneration/Generation/PackingServices/PostProcessing/Source/RattlerRemovalService.d 
+./PackingGeneration/Generation/PackingServices/PostProcessing/Source/RattlerRemovalService.d \
+./PackingGeneration/Generation/PackingServices/PostProcessing/Source/ScatterAndDiffusionProcessor.d \
+./PackingGeneration/Generation/PackingServices/PostProcessing/Source/SelfDiffusionProcessor.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
